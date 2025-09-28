5d01a0e12137cb317e9c6bd7ec2f73300a70081e
Syed Syab Ahmad <81256221+SyabAhmad@users.noreply.github.com>
2025-09-28 11:59:03 +0500

    Add src/pages/About.tsx

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div className="page">
+      <h1>About</h1>
+      <p>This is the about page of your React application.</p>
+    </div>
+  )
+}
+
+export default About
